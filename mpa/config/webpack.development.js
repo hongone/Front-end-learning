35b0d6aee194856e9b19841bad547d9e74b2d7ce
hongone <6rl@163.com>
2018-11-14 13:04:04 +0800

    编写copywebpackplugin的配置

--- mpa/config/webpack.development.js
+++ mpa/config/webpack.development.js
@@ -1,8 +1,22 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const {join} = require('path');
 module.exports ={
     output :{
         path: join(__dirname,'../dist/assets'),
         publicPath: '/',
         filename : "scripts/[name].bundles.js"
-    }
+    },
+    plugins :[
+        new CopyWebpackPlugin([ {
+            from : join(__dirname,"../src/webapp/views/common/layout.html"),
+            to : "../views/common/layout.html"
+        },
+        {
+            from : join(__dirname,"../src/webapp/components"),to : "../components"
+        },
+        ]
+        , {copyUnmodified : true,//只打包copy内容
+            ignore :["*.js","*.css"]
+        })
+    ]
 }

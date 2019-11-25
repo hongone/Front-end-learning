96fbfc24386f863269335bb4330008e76ea3865b
hongone <6rl@163.com>
2019-11-26 06:54:43 +0800

    'vue-csr'

---
+++ vue/vue-csr/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <Header/>
+        <Footer/>
+    </div>
+</template>
+
+<script>
+import Header from './components/header';
+import Footer from './components/footer';
+export default {
+    compontents: {
+        Header, Footer
+    } 
+    
+}
+</script>
---
+++ vue/vue-csr/src/components/footer.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="footer">
+        页脚
+    </div>
+</template>
+
+<style lang="stylus" scoped>
+    .footer {
+        background: yellowgreen;
+    }
+</style>
+
+<script>
+import Header from './components/header';
+import Footer from './components/footer';
+
+export default {
+    compontents: {
+        Header, Footer
+    } 
+    
+}
+</script>

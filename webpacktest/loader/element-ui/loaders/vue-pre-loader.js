dddef49bcf565991c4674f7e6cdb585b066cbb7e
hongone <6rl@163.com>
2020-04-22 06:53:44 +0800

    处理e-table标签的ref属性

--- webpacktest/loader/element-ui/loaders/vue-pre-loader.js
+++ webpacktest/loader/element-ui/loaders/vue-pre-loader.js
@@ -21,30 +21,45 @@ module.exports = function(source) {
     const options = getOptions(this);
     // console.log('--s')
     // console.log(source)
-     // 在这里写转换 source 的逻辑 ...
-    // const reg = /[/s/S]*<template>([/s/S]*?)<\/template>[/s/S]*/g
-    const reg = /<template>([\s\S]*)<\/template>/g
-    const reg2 = /<template>[\s\S]*<\/template>/
+    // 在这里写转换 source 的逻辑 ...
+   
+    const tableRefs = [];
+    const regT = /<template>([\s\S]*)<\/template>/g
+    const regS = /<script>([\s\S]*)<\/script>/g
+    // const reg2 = /<template>[\s\S]*<\/template>/
     // const reg = /(.*?)/g
-    reg.test(source)
-    // console.log(reg.test(source))
 
-    // console.log(RegExp.$1)
+    // 模板处理块
+    regT.test(source)
     const $ = cheerio.load(RegExp.$1)
-    console.log('ref', $('el-table').eq(0).attr('ref'))
+    const tables = $('el-table');
+    
+    tables.each(function(index, el) {
 
-    $('el-table').eq(0).attr('ref', 'table1')
+      var refValue = undefined
+      refValue = $(this).attr('ref') ? $(this).attr('ref') : 'etable_' + index;
+      if (!$(this).attr('ref')){
+        $(this).attr('ref', refValue) 
+      }
+      tableRefs.push(refValue)
+      console.log(refValue);
+      // console.log($(this).nodeName+':'+$(this).nodeValue);
+    });
+
+    // $('el-table').eq(0).attr('ref', 'table1')
     
     // var obj=$("#el-table").eq(0).attr();
-    //  console.log(obj ); 
-//     $.each(obj,function(){
-//       console.log(vobj );      
-//     });
-    // console.log($('body').html())
+    //  console.log($("el-table").eq(0).attributes); 
+
     const newTemplate = '<template>' + $('body').html() + '</template>'
     // console.log(newTemplate)
     // console.log(source.replace(reg2, newTemplate))
     source = source.replace(reg2, newTemplate)
+
+    // 脚本处理块
+    regS.test(source)
+    // const $ = cheerio.load(RegExp.$1)
+
     console.log(source)
     return source;
 };
--- webpacktest/loader/element-ui/src/App.vue
+++ webpacktest/loader/element-ui/src/App.vue
@@ -1,22 +1,66 @@
 <template>
-  <el-table
-    :data="tableData"
-    style="width: 100%">
-    <el-table-column
-      prop="date"
-      label="日期"
-      width="180">
-    </el-table-column>
-    <el-table-column
-      prop="name"
-      label="姓名"
-      width="180">
-    </el-table-column>
-    <el-table-column
-      prop="address"
-      label="地址">
-    </el-table-column>
-  </el-table>
+  <div>
+    <el-table
+      :data="tableData"
+      border
+      show-summary
+      style="width: 100%">
+      <el-table-column
+        prop="id"
+        label="ID"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="姓名">
+      </el-table-column>
+      <el-table-column
+        prop="amount1"
+        sortable
+        label="数值 1">
+      </el-table-column>
+      <el-table-column
+        prop="amount2"
+        sortable
+        label="数值 2">
+      </el-table-column>
+      <el-table-column
+        prop="amount3"
+        sortable
+        label="数值 3">
+      </el-table-column>
+    </el-table>
+
+    <el-table
+      :data="tableData"
+      border
+      height="200"
+      :summary-method="getSummaries"
+      show-summary
+      style="width: 100%; margin-top: 20px">
+      <el-table-column
+        prop="id"
+        label="ID"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="姓名">
+      </el-table-column>
+      <el-table-column
+        prop="amount1"
+        label="数值 1（元）">
+      </el-table-column>
+      <el-table-column
+        prop="amount2"
+        label="数值 2（元）">
+      </el-table-column>
+      <el-table-column
+        prop="amount3"
+        label="数值 3（元）">
+      </el-table-column>
+    </el-table>
+  <div>
 </template>
 
 <script>
@@ -24,23 +68,65 @@
     data() {
       return {
         tableData: [{
-          date: '2016-05-02',
+          id: '12987122',
           name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
+          amount1: '234',
+          amount2: '3.2',
+          amount3: 10
         }, {
-          date: '2016-05-04',
+          id: '12987123',
           name: '王小虎',
-          address: '上海市普陀区金沙江路 1517 弄'
+          amount1: '165',
+          amount2: '4.43',
+          amount3: 12
         }, {
-          date: '2016-05-01',
+          id: '12987124',
           name: '王小虎',
-          address: '上海市普陀区金沙江路 1519 弄'
+          amount1: '324',
+          amount2: '1.9',
+          amount3: 9
         }, {
-          date: '2016-05-03',
+          id: '12987125',
           name: '王小虎',
-          address: '上海市普陀区金沙江路 1516 弄'
+          amount1: '621',
+          amount2: '2.2',
+          amount3: 17
+        }, {
+          id: '12987126',
+          name: '王小虎',
+          amount1: '539',
+          amount2: '4.1',
+          amount3: 15
         }]
+      };
+    },
+    methods: {
+      getSummaries(param) {
+        const { columns, data } = param;
+        const sums = [];
+        columns.forEach((column, index) => {
+          if (index === 0) {
+            sums[index] = '总价';
+            return;
+          }
+          const values = data.map(item => Number(item[column.property]));
+          if (!values.every(value => isNaN(value))) {
+            sums[index] = values.reduce((prev, curr) => {
+              const value = Number(curr);
+              if (!isNaN(value)) {
+                return prev + curr;
+              } else {
+                return prev;
+              }
+            }, 0);
+            sums[index] += ' 元';
+          } else {
+            sums[index] = 'N/A';
+          }
+        });
+
+        return sums;
       }
     }
-  }
+  };
 </script>

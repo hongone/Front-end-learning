5ca2485c982b182a42559e54208b92cc1a000aa0
hongone <6rl@163.com>
2020-07-12 18:44:07 +0800

    移动端css学习

---
+++ mobile/ilearn/gulpfile.js
@@ -0,0 +1,37 @@
+//导入工具包 require('node_modules里对应模块')
+const { series, src, dest, watch } = require('gulp');
+const less = require('gulp-less');
+const lessFiles = './src/css/**/*.less';
+
+const distdDir = './dist';
+ 
+const htmlFiles = './src/**/*.html';
+const cssFiles = './src/**/*.css';
+
+ // 开发环境的gulp
+function builddev() {
+  return watch([lessFiles,htmlFiles,cssFiles], { ignoreInitial: false },
+    () => {
+      src(htmlFiles)
+        .pipe(dest(distdDir));
+      src(cssFiles)
+        .pipe(dest(distdDir));
+      src(lessFiles)
+        .pipe(less())
+        .pipe(dest(distdDir));
+      return 
+  })
+}
+
+function copydev() {
+  return watch([sourceFiles], { ignoreInitial: false },
+    () => {
+      return src(sourceFiles)
+        .pipe(less())
+        .pipe(dest(distdDir));
+  })
+}
+
+let _task = series(builddev)
+
+exports.default = _task
